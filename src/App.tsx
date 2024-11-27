115af64844d7ee0215771468a61f71ad928287c9
BroccoliGuy <votre.email@example.com>
2024-11-27 04:40:14 +0100

    ''

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Lobby from "./pages/Lobby";
 import { LanguageProvider } from "../src/utils/LanguageContext"; // Import du contexte de langue
